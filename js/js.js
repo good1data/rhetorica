f843fd2cf120c1f5b2551f3d66fdc91fad59312d
good1data <good1data@gmail.com>
2022-04-26 00:16:45 +0500

    меню

--- js/js.js
+++ js/js.js
@@ -1,8 +1,140 @@
 // JavaScript Document
 window.onload = function() {
-	
+	// МЕНЮ НАВИГАЦИЯ 
 		// переменные
-		let rightMenu = document.querySelector('#rightMenu'); // выбрали контейнер меню
+		let pointsMenu = document.querySelectorAll('.menuPoint'); // выбрали все пункты меню
+		let arrPointsMenu = Array.from(pointsMenu); // сформировали из пунктов массив
+		let lineMenu = document.querySelectorAll('.menuLine'); // выбрали все линии меню
+		let arrlineMenu = Array.from(lineMenu); // сформировали из линий массив
+		let menuTextAll = document.querySelectorAll('.menuText'); // выбрали все всплывающие подсказку в меню
+		let arrmenuTextAll = Array.from(menuTextAll); // сформировали из всплывающих пунктов массив
+
+		console.log(arrPointsMenu);
+
+		arrPointsMenu.forEach(function(menuPoint, index) { // каждому пункту меню назначили событие
+			menuPoint.addEventListener("mouseover", function(event){ // событие при наведении на пункт меню
+				let selectPointClass = event.target.classList; // получаем список классов пункта меню
+				let arrselectPointClass = Array.from(selectPointClass); // формируем массив из списока классов пункта меню
+				let findMenuPointMove = arrselectPointClass.includes('menuPoint_Move'); // записываем в переменную есть ли в массиве классов элемента класс активности
+	
+				arrmenuTextAll[index].classList.add('menuText_Move'); // показываем подсказку
+
+				if(findMenuPointMove == false){ // если класса активности нет			
+					let upLineMenuIndex = index; // переменная для хранения индекса линии выше выбранного пункта меню
+					let downLineMenuIndex = index + 1; // переменная для хранения индекса линии ниже выбранного пункта меню
+					arrlineMenu[upLineMenuIndex].classList.add('moveLine'); // добавляем класс наведения на линию
+					arrlineMenu[downLineMenuIndex].classList.add('moveLine');// добавляем класс наведения на линию
+				}
+			})
+
+			menuPoint.addEventListener("mouseout", function(event){ // событие при уведения на пункт меню
+				let selectPointClass = event.target.classList; // получаем список классов пункта меню
+				let arrselectPointClass = Array.from(selectPointClass); // формируем массив из списока классов пункта меню
+				let findMenuPointMove = arrselectPointClass.includes('menuPoint_Move'); // записываем в переменную есть ли в массиве классов элемента класс активности
+			
+				arrmenuTextAll[index].classList.remove('menuText_Move'); // прячем подсказку
+
+				if(findMenuPointMove == false){ // если класса активности нет
+					let upLineMenuIndex = index; // переменная для хранения индекса линии выше выбранного пункта меню
+					let downLineMenuIndex = index + 1; // переменная для хранения индекса линии ниже выбранного пункта меню
+					arrlineMenu[upLineMenuIndex].classList.remove('moveLine'); // добавляем класс наведения на линию
+					arrlineMenu[downLineMenuIndex].classList.remove('moveLine');// добавляем класс наведения на линию
+				}
+			})
+		})
+	// /МЕНЮ НАВИГАЦИЯ
+
+
+
+	const options = {
+        // родитель целевого элемента - область просмотра
+        root: null,
+        // без отступов
+        rootMargin: '0px',
+        // процент пересечения - половина изображения
+        threshold: 0.1
+    }
+
+    // создаем наблюдатель
+    const observer = new IntersectionObserver((entries, observer) => {
+		
+        // для каждой записи-целевого элемента
+        entries.forEach(entry => {
+            // если элемент является наблюдаемым 
+            if (entry.isIntersecting) {
+                let visibleScreen = entry.target;
+				let targetMenuPointIndex = visibleScreen.dataset.screennamber;
+				arrPointsMenu[targetMenuPointIndex].classList.add('menuPoint_active'); 
+				let upLineMenuIndex = Number(targetMenuPointIndex); // переменная для хранения индекса линии выше выбранного пункта меню
+				let downLineMenuIndex = Number(targetMenuPointIndex) + 1; // переменная для хранения индекса линии ниже выбранного пункта меню
+				arrlineMenu[upLineMenuIndex].classList.add('moveLine'); // добавляем класс наведения на линию
+				console.log(downLineMenuIndex);
+				arrlineMenu[downLineMenuIndex].classList.add('moveLine');// добавляем класс наведения на линию 
+				arrPointsMenu[targetMenuPointIndex].childNodes[1].classList.add('menuPoint__inside_active'); 
+				console.log(visibleScreen.clientHeight);
+				
+
+				
+
+            } else {
+				let visibleScreen = entry.target;
+				let targetMenuPointIndex = visibleScreen.dataset.screennamber;
+				arrPointsMenu[targetMenuPointIndex].classList.remove('menuPoint_active'); 
+				let upLineMenuIndex = Number(targetMenuPointIndex); // переменная для хранения индекса линии выше выбранного пункта меню
+				let downLineMenuIndex = Number(targetMenuPointIndex) + 1; // переменная для хранения индекса линии ниже выбранного пункта меню
+				arrlineMenu[upLineMenuIndex].classList.remove('moveLine'); // добавляем класс наведения на линию
+				arrlineMenu[downLineMenuIndex].classList.remove('moveLine');// добавляем класс наведения на линию 
+				arrPointsMenu[targetMenuPointIndex].childNodes[1].classList.remove('menuPoint__inside_active'); 
+			}
+        })
+    }, options)
+
+    // с помощью цикла следим за всеми img на странице
+    const arr = document.querySelectorAll('.pageIdentification')
+    arr.forEach(i => {
+        observer.observe(i)
+    })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -20,16 +152,19 @@ window.onload = function() {
 		let downLine; // переменная для хранения линии ниже выбранного пункта меню
 		
 		let sectionPage = document.getElementById('advantagesScreen');
-	*/
+	
 	
 	
 		// наведение на пункт меню
-		rightMenu.addEventListener("mouseover", function(event) { //срабатывает при наведении на пункт меню
+		rightMenu.addEventListener("mouseover", function(event) { //срабатывает при наведении на область меню
 			
 			
-			let selectPoint = event.target;
-			let selectPointClass = selectPoint.classList;
-			console.log(selectPointClass);
+			let selectPoint = event.target; // выбрали элемент на котором сработал
+			let selectPointClass = selectPoint.classList; //выбрали классы которые есть в выбранном элементе
+			let arrselectPointClass = Array.from(selectPointClass); // сформировали все классы в массив
+
+			console.log(arrselectPointClass);
+
 
 		});
 			/*
