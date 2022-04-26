0bc49a972124b555d61b35d80669d094c682d8b4
good1data <good1data@gmail.com>
2022-04-26 14:26:54 +0500

    js готов вроде

--- js/js.js
+++ js/js.js
@@ -1,5 +1,9 @@
 // JavaScript Document
 window.onload = function() {
+	let screensPages = document.querySelectorAll('.pageIdentification');
+	let arrScreensPages = Array.from(screensPages); // сформировали пуктов меню в массив
+	let scrollPageStatus = true; // статус постраничного скрола
+
 	// МЕНЮ НАВИГАЦИЯ 
 		// переменные
 		let pointsMenu = document.querySelectorAll('.menuPoint'); // выбрали все пункты меню
@@ -12,7 +16,21 @@ window.onload = function() {
 		console.log(arrPointsMenu);
 
 		arrPointsMenu.forEach(function(menuPoint, index) { // каждому пункту меню назначили событие
+
+			menuPoint.addEventListener("click", function(event){ // событие при клике на пункт меню
+				function stopPageScroll(){
+					scrollPageStatus = true;
+				}
+
+				scrollPageStatus = false;
+				setTimeout(stopPageScroll, 1000);
+	
+			})
+
+
+
 			menuPoint.addEventListener("mouseover", function(event){ // событие при наведении на пункт меню
+				
 				let selectPointClass = event.target.classList; // получаем список классов пункта меню
 				let arrselectPointClass = Array.from(selectPointClass); // формируем массив из списока классов пункта меню
 				let findMenuPointMove = arrselectPointClass.includes('menuPoint_Move'); // записываем в переменную есть ли в массиве классов элемента класс активности
@@ -46,24 +64,39 @@ window.onload = function() {
 
 
 
+
 	const options = {
         // родитель целевого элемента - область просмотра
         root: null,
         // без отступов
         rootMargin: '0px',
         // процент пересечения - половина изображения
-        threshold: 0.1
+        threshold: 0.01
     }
 
     // создаем наблюдатель
     const observer = new IntersectionObserver((entries, observer) => {
-		
+	
         // для каждой записи-целевого элемента
         entries.forEach(entry => {
-            // если элемент является наблюдаемым 
-            if (entry.isIntersecting) {
-                let visibleScreen = entry.target;
+            
+            if (entry.isIntersecting) {// если элемент является наблюдаемым 
+                
+				let nextPage = arrScreensPages.findIndex(item => item.id == entry.target.id);
+				console.log(nextPage + ' перемотка');
+				let scrollPage = entry.target.offsetHeight * nextPage;
+				console.log(scrollPage + ' перемотка2');
+				if(scrollPageStatus == true){
+					window.scrollTo(0, scrollPage);	
+				}
+				
+				
+				let visibleScreen = entry.target;
 				let targetMenuPointIndex = visibleScreen.dataset.screennamber;
+
+				let detectMenuPoint_active = arrPointsMenu[targetMenuPointIndex].classList.contains('menuPoint_active');  // активен ли пункт меню
+				console.log(detectMenuPoint_active);
+
 				arrPointsMenu[targetMenuPointIndex].classList.add('menuPoint_active'); 
 				let upLineMenuIndex = Number(targetMenuPointIndex); // переменная для хранения индекса линии выше выбранного пункта меню
 				let downLineMenuIndex = Number(targetMenuPointIndex) + 1; // переменная для хранения индекса линии ниже выбранного пункта меню
@@ -71,27 +104,75 @@ window.onload = function() {
 				console.log(downLineMenuIndex);
 				arrlineMenu[downLineMenuIndex].classList.add('moveLine');// добавляем класс наведения на линию 
 				arrPointsMenu[targetMenuPointIndex].childNodes[1].classList.add('menuPoint__inside_active'); 
-				console.log(visibleScreen.clientHeight);
 				
+				if( visibleScreen.dataset.screennamber != 1 && arrPointsMenu[1].classList.contains('menuPoint_active') == true ){
+					arrPointsMenu[1].classList.remove('menuPoint_active');
+					arrPointsMenu[1].childNodes[1].classList.remove('menuPoint__inside_active'); 
+					arrlineMenu[1].classList.remove('moveLine'); // удаляем  класс наведения на линию
+					arrlineMenu[2].classList.remove('moveLine');// удаляем класс наведения на линию 
+				}
+
+				if( visibleScreen.dataset.screennamber != 3 && arrPointsMenu[3].classList.contains('menuPoint_active') == true ){
+					arrPointsMenu[3].classList.remove('menuPoint_active');
+					arrPointsMenu[3].childNodes[1].classList.remove('menuPoint__inside_active'); 
+					arrlineMenu[3].classList.remove('moveLine'); // удаляем  класс наведения на линию
+					arrlineMenu[4].classList.remove('moveLine');// удаляем класс наведения на линию 
+				}
+				if( visibleScreen.dataset.screennamber != 7 && arrPointsMenu[7].classList.contains('menuPoint_active') == true ){
+					arrPointsMenu[7].classList.remove('menuPoint_active');
+					arrPointsMenu[7].childNodes[1].classList.remove('menuPoint__inside_active'); 
+					arrlineMenu[7].classList.remove('moveLine'); // удаляем  класс наведения на линию
+					arrlineMenu[8].classList.remove('moveLine');// удаляем класс наведения на линию 
+				}
 
 				
 
-            } else {
+				//console.log(visibleScreen.clientHeight);
+				console.log('наблюдаем ' + targetMenuPointIndex);
+				console.log('наблюдаем ' + visibleScreen.id);
+				
+				//window.scrollTo(0, 0);
+
+				
+
+            } else {// если элемент не является наблюдаемым 
+				let adjustmentStatusMenu = true;
+
 				let visibleScreen = entry.target;
 				let targetMenuPointIndex = visibleScreen.dataset.screennamber;
-				arrPointsMenu[targetMenuPointIndex].classList.remove('menuPoint_active'); 
-				let upLineMenuIndex = Number(targetMenuPointIndex); // переменная для хранения индекса линии выше выбранного пункта меню
-				let downLineMenuIndex = Number(targetMenuPointIndex) + 1; // переменная для хранения индекса линии ниже выбранного пункта меню
-				arrlineMenu[upLineMenuIndex].classList.remove('moveLine'); // добавляем класс наведения на линию
-				arrlineMenu[downLineMenuIndex].classList.remove('moveLine');// добавляем класс наведения на линию 
-				arrPointsMenu[targetMenuPointIndex].childNodes[1].classList.remove('menuPoint__inside_active'); 
+				if(visibleScreen.dataset.screennamber == 1 && arrPointsMenu[1].classList.contains('menuPoint_active') == true ){
+					
+					adjustmentStatusMenu = false;
+				}
+				if(visibleScreen.dataset.screennamber == 3 && arrPointsMenu[3].classList.contains('menuPoint_active') == true ){
+					
+					adjustmentStatusMenu = false;
+				}
+				console.log('hhhhhh ' + visibleScreen.dataset.screennamber)
+				if(visibleScreen.dataset.screennamber == 7 && arrPointsMenu[7].classList.contains('menuPoint_active') == true ){
+					
+					adjustmentStatusMenu = false;
+				}
+
+				if(adjustmentStatusMenu == true){
+					arrPointsMenu[targetMenuPointIndex].classList.remove('menuPoint_active'); 
+					let upLineMenuIndex = Number(targetMenuPointIndex); // переменная для хранения индекса линии выше выбранного пункта меню
+					let downLineMenuIndex = Number(targetMenuPointIndex) + 1; // переменная для хранения индекса линии ниже выбранного пункта меню
+					arrlineMenu[upLineMenuIndex].classList.remove('moveLine'); // удаляем  класс наведения на линию
+					arrlineMenu[downLineMenuIndex].classList.remove('moveLine');// удаляем класс наведения на линию 
+					arrPointsMenu[targetMenuPointIndex].childNodes[1].classList.remove('menuPoint__inside_active'); 
+				}
+
+
+
+				console.log('вышел из наблюдения ' + visibleScreen.id);
 			}
         })
     }, options)
 
     // с помощью цикла следим за всеми img на странице
-    const arr = document.querySelectorAll('.pageIdentification')
-    arr.forEach(i => {
+    const screens = document.querySelectorAll('.pageIdentification')
+    screens.forEach(i => {
         observer.observe(i)
     })
 
